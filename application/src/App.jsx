3817996c1123ee5cb950a5e54944a47ee3c22450
Akshat <akshatmiglani.am@gmail.com>
2025-01-03 01:22:57 +0530

    Todo application created, appsec file , initialization scripts and cft template and workflow file

---
+++ application/src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+
+    const newTask = event.target.task.value.trim();
+
+    if (!newTask) {
+      alert('Task cannot be empty!');
+      return;
+    }
+
+    if (!tasks.includes(newTask)) {
+      setTasks([...tasks, newTask]); 
+      event.target.reset(); 
+    } else {
+      alert('Task already exists!');
+    }
+  };
+
+  const taskListItems = tasks.map((task, index) => (
+    <TaskItem 
+      key={index} 
+      task={task} 
+      index={index} 
+      tasks={tasks} 
+      setTasks={setTasks} 
+    />
+  ));
+
+  return (
+    <>
+      <h1>Task Manager</h1>
+      <div>
+        <form onSubmit={handleAddTask}>
+          <input type='text' name='task' placeholder='Enter a task...' />
+          <button type='submit'>Add Task</button>
+        </form>
+
+        <div>
+          <ul>{taskListItems}</ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
+
+
+function TaskItem({ task, tasks, setTasks, index }) {
+  const handleDeleteTask = () => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <li className='task-item'>
+      {task} <span className='delete-btn' onClick={handleDeleteTask}>&times;</span>
+    </li>
+  );
+}
